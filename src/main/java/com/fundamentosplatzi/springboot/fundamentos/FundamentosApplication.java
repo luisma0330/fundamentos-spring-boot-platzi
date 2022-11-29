d54278864601966452105736edc35cdb68baf027
Luis Ojeda <luisma0330@hotmail.com>
2022-11-28 19:11:24 -0600

    Agregando el server port y el path, ademas de añadir nuestra propia dependencia

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,5 +1,7 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -11,8 +13,14 @@ public class FundamentosApplication implements CommandLineRunner {
 
 	private ComponentDependency componentDependency;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency) {
+	private MyBean myBean;
+
+	private MyBeanWithDependency myBeanWithDependency;
+
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency) {
 		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+		this.myBeanWithDependency = myBeanWithDependency;
 	}
 
 	public static void main(String[] args) {
@@ -22,5 +30,7 @@ public class FundamentosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		componentDependency.saludar();
+		myBean.print();
+		myBeanWithDependency.printWithDependency();
 	}
 }
